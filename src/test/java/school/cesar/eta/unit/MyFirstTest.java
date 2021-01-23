6a914b964a1a75a09d0af0b15624662beb40cd37
Milton Castro <miltonmc@gmail.com>
2021-01-23 09:52:25 -0300

    My first test

---
+++ src/test/java/school/cesar/eta/unit/MyFirstTest.java
@@ -0,0 +1,12 @@
+package school.cesar.eta.unit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void myFirsTest(){
+        Assertions.assertEquals(true, true);
+    }
+}
